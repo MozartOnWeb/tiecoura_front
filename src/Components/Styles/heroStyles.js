4231e3504fa309c8c6132e5eb109c315402cc056
MozartOnWeb <mozartontheweb@gmail.com>
2021-09-27 23:53:37 +0200

    tiecoura

--- src/Components/Styles/heroStyles.js
+++ src/Components/Styles/heroStyles.js
@@ -1,8 +1,7 @@
 import styled from "styled-components";
 import { only, down } from "styled-breakpoints";
-import { motion } from "framer-motion";
 
-export const BackgroundImage = styled(motion.div)`
+export const BackgroundImage = styled.div`
   width: 100vw;
   height: 100vh;
   overflow: hidden;
